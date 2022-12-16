46838c2f8ea9642a72874086aab5ecc06555684f
likhit <likhitgiradkar1995@gmail.com>
2022-12-16 22:33:13 +0530

    Added code for add transaction

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [form, setForm] = useState({ amount: 0, description: "", date: "" });
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:4000/transaction", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log("data >> ", data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="amount"
+          value={form.amount}
+          onChange={handleInput}
+          placeholder="Enter transaction amount"
+        />
+        <input
+          type="text"
+          name="description"
+          value={form.description}
+          onChange={handleInput}
+          placeholder="Enter description"
+        />
+        <input
+          type="date"
+          name="date"
+          value={form.date}
+          onChange={handleInput}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,32 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import Transaction from "./models/Transaction.js";
+
 const PORT = 4000;
 const app = express();
+app.use(cors());
+app.use(bodyParser());
+
+await mongoose.connect(
+  "mongodb+srv://likhit:likhit1995%40@cluster0.0dq6uzc.mongodb.net/test"
+);
+console.log("mongo connected");
 
 app.get("/", (req, res) => {
   res.send("Hello World !");
+});
+
+app.post("/transaction", async (req, res) => {
+  const { amount, description, date } = req.body;
+  const transaction = new Transaction({
+    amount,
+    description,
+    date,
+  });
+  await transaction.save();
+  res.json({ message: "success" });
 });
 
 app.listen(PORT, () => {
